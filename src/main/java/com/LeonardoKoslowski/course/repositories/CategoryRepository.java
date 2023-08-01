849753da53c7161ec8cf699657eb2c9782238ea5
Leonardo Koslowski <koslowskileonardo84@gmail.com>
2023-08-01 19:32:15 -0300

    Category implemented

---
+++ src/main/java/com/LeonardoKoslowski/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.LeonardoKoslowski.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.LeonardoKoslowski.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
