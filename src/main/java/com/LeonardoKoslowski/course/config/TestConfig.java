526fa547698c6c2ea82f1ab6ecf7e09b32f43d53
Leonardo Koslowski <koslowskileonardo84@gmail.com>
2023-08-02 20:24:48 -0300

    Payment, one-to-one association implemented

--- src/main/java/com/LeonardoKoslowski/course/config/TestConfig.java
+++ src/main/java/com/LeonardoKoslowski/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.LeonardoKoslowski.course.entities.Category;
 import com.LeonardoKoslowski.course.entities.Order;
 import com.LeonardoKoslowski.course.entities.OrderItem;
+import com.LeonardoKoslowski.course.entities.Payment;
 import com.LeonardoKoslowski.course.entities.Product;
 import com.LeonardoKoslowski.course.entities.User;
 import com.LeonardoKoslowski.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+				
 	}
 	
 	
